45364d18faaff6e723c0eda720d8971c82f62eb5
Anderson de Oliveira <anderson.o@gmail.com>
2012-03-01 19:06:55 -0300

    adicionado email

--- sql/users.sql
+++ sql/users.sql
@@ -2,6 +2,7 @@ create table usuarios (
 	id int(10) unsigned not null auto_increment,
 	username varchar(20) not null,
 	password varchar (32) not null,
+	email varchar (64),
 	type varchar(16) not null,
 	primary key (id)
 );
